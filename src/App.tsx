548a8936807a054aab128cf78bb494eb6683bfc9
VladimirSazanakov <sazanakov@mail.ru>
2023-09-29 01:11:56 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <LandingBuilder />
-      <SliderSVPage />
+      {/* <SliderSVPage /> */}
       {/* <MainPage /> */}
       {/* <CvTemplate /> */}
     </>
